fddcb22f824c8fb6c88b5c0acbc250098ddd1656
Haythem <sinnner6@gmail.com>
2024-11-21 10:52:32 +0100

    setup default layout

---
+++ layouts/default.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-app>
+        <v-layout>
+            <!-- NAVIGATION DRAWER  -->
+            <v-navigation-drawer v-model="drawer"
+            class="bg-indigo"
+            theme="dark"
+            permanent
+            >
+            <v-list density="compact" item-props nav>
+                <v-list-subheader>ADMINISTRATION</v-list-subheader>
+                <v-list-item 
+                v-for="item in items"
+                :key="item.title"
+                :title="item.title"
+                :prepend-icon="item.prependIcon"
+                @click="navigateToPage(item.url)"
+                />
+            </v-list>
+            </v-navigation-drawer>
+
+            <!-- APP BAR  -->
+            <v-app-bar class="ps-4 bg-indigo-darken-1" flat>
+                <v-app-bar-nav-icon v-if="$vuetify.display.smAndDown" @click="drawer = !drawer" />
+                <v-app-bar-title>Application de Gestion</v-app-bar-title>
+                <template #append>
+                    <v-btn class="text-none me-2" height="48" icon slim>
+                        <v-avatar color="surface-light" image="https://robohash.org/vuejs" size="32" />
+
+                        <v-menu activator="parent">
+                            <v-list density="compact" nav>
+                                <v-list-item append-icon="mdi-cog-outline" link title="Options" @click="navigateToPage('/settings')"/>
+                                <v-list-item append-icon="mdi-logout" link title="Se déconnecter" @click="logOut" />
+                            </v-list>
+                        </v-menu>
+                    </v-btn>
+                </template>
+            </v-app-bar>
+
+            <v-main>
+                <div class="pa-4" v-if="showContent">
+                    <slot/>
+                </div>
+            </v-main>
+        </v-layout>
+    </v-app>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+
+// vars
+const drawer = ref(true)
+const items = ref([
+    {
+        title: 'Dashboard',
+        prependIcon: 'mdi-view-dashboard-outline',
+        link: true,
+        url: "/"
+    },
+    {
+        title: 'Team',
+        prependIcon: 'mdi-account-group',
+        link: true,
+        url: "/team"
+    },
+    {
+        title: 'Projects',
+        prependIcon: 'mdi-briefcase-outline',
+        link: true,
+        url: "/projects"
+    },
+    {
+        title: 'Reports',
+        prependIcon: 'mdi-file-chart-outline',
+        link: true,
+        url: "/reports"
+    },
+])
+const showContent = ref(true)
+
+// methods
+const navigateToPage = async (page: any) => {
+    await navigateTo(page)
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <h1>Accueil</h1>
+  <v-card
+    class="mx-auto"
+    max-width="368"
+    color="indigo"
+  >
+    <v-card-item title="Utilisateurs">
+      <template v-slot:subtitle>
+        <v-icon
+          class="me-1 pb-1"
+          color="success"
+          icon="mdi-trending-up"
+          size="18"
+        ></v-icon>
+
+        Extreme Weather Alert
+      </template>
+    </v-card-item>
+
+    <v-card-text class="py-0">
+      <v-row align="center" no-gutters>
+        <v-col
+          class="text-h2"
+          cols="6"
+        >
+          42
+        </v-col>
+
+        <v-col class="text-right" cols="6">
+          <v-icon
+            icon="mdi-account"
+            size="88"
+          ></v-icon>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <div class="d-flex py-3 justify-space-between">
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-windy"
+      >
+        <v-list-item-subtitle>123 km/h</v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item
+        density="compact"
+        prepend-icon="mdi-weather-pouring"
+      >
+        <v-list-item-subtitle>48%</v-list-item-subtitle>
+      </v-list-item>
+    </div>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn>TODO</v-btn>
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<script lang="ts" setup>
+    const { data } = await useFetch('http://localhost:3002/users')
+</script>
