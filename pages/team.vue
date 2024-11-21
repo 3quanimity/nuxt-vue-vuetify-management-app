8256ceda8016661396b1a7d3f10581e1455f5621
Haythem <sinnner6@gmail.com>
2024-11-21 15:26:27 +0100

    list users in a data table (vuetify comp)

---
+++ pages/team.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-sheet class="mx-auto" width="100%">
+        <h1>L'équipe</h1>
+        <v-spacer/>
+        <v-data-table :items="usersData" v-if="usersData"></v-data-table>
+    </v-sheet>
+</template>
+
+<script lang="ts" setup>
+  const { data : usersData } = await useFetch("http://localhost:3002/users");
+</script>
