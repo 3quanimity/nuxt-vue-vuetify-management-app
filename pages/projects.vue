207b9a37810535916390c62b9237c56adf520474
Haythem <sinnner6@gmail.com>
2024-11-22 11:11:57 +0100

    add a new project through form, with vee-validate for form validation

--- pages/projects.vue
+++ pages/projects.vue
@@ -8,21 +8,53 @@
         <ProjectSingle :project="project"></ProjectSingle>
       </template>
     </v-row>
+
+    <v-divider class="mt-8"></v-divider>
+
+    <!-- Crete new project  -->
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-title class="text-h4"
+          >Ajouter un projet</v-expansion-panel-title
+        >
+        <v-expansion-panel-text>
+          <form @submit.prevent="submit">
+            <v-text-field
+              v-model="name.value.value"
+              :counter="3"
+              :error-messages="name.errorMessage.value"
+              label="Nom"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="desc.value.value"
+              :error-messages="desc.errorMessage.value"
+              label="Description"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="user.value.value"
+              :counter="1"
+              :error-messages="user.errorMessage.value"
+              label="Utilisateur"
+            ></v-text-field>
+
+            <v-btn class="me-4" type="submit"> Valider </v-btn>
+
+            <v-btn @click="handleReset"> Réinitialiser </v-btn>
+          </form>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </v-sheet>
 </template>
 
 <script lang="ts" setup>
+import { useField, useForm } from "vee-validate";
+
 interface Project {
   id: string;
 }
-
-const { data, status, error, refresh } = await useFetch<Project[]>(
-  "http://localhost:3002/projects"
-);
-
-const getRandomImage = () => {
-  return "https://random.imagecdn.app/300/240";
-};
 
 const projectsWithImages = computed(() => {
   if (!data.value) return [];
@@ -32,4 +64,54 @@ const projectsWithImages = computed(() => {
     image: getRandomImage(),
   }));
 });
+
+const { data, status, error, refresh } = await useFetch<Project[]>(
+  "http://localhost:3002/projects"
+);
+
+const getRandomImage = () => {
+  return "https://random.imagecdn.app/300/240";
+};
+
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: {
+    name(value: any) {
+      if (value?.length >= 3) return true;
+      return "Le nom doit contenir au moins 3 caractères";
+    },
+    desc(value: any) {
+      if (value?.length >= 3) return true;
+      return "Le nom doit contenir au moins 3 caractèes";
+    },
+    user(value: any) {
+      if (/^[0-9-]{1,}$/.test(value)) return true;
+      return "Il faut au moins 1 chiffre";
+    },
+  },
+});
+
+const name = useField("name");
+const desc = useField("desc");
+const user = useField("user");
+
+const submit = handleSubmit((values) => {
+  //   alert(JSON.stringify(values, null, 2));
+  const projectToCreate = JSON.stringify(values, null, 2);
+  createProject(projectToCreate);
+});
+
+const createProject = async (project: any) => {
+  const { data: responseData } = await useFetch(
+    `http://localhost:3002/projects`,
+    {
+      method: "post",
+      body: project,
+    }
+  );
+
+  if (responseData.value) {
+    handleReset();
+    refresh();
+  }
+};
 </script>
